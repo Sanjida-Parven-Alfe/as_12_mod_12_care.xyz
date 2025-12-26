86fb37df79ad9ac86d0732f874ec7f9a132ddf17
Sanjida Parven Alfe <sanjidaparvinalfe@gmail.com>
2025-12-26 21:55:25 +0600

    Page update

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,18 @@
-import Image from "next/image";
+import AboutSection from "@/components/home/AboutSection";
+import Banner from "@/components/home/Banner";
+import ServicesSection from "@/components/home/ServicesSection";
+import TestimonialsSection from "@/components/home/TestimonialsSection";
 
 export default function Home() {
   return (
-    <div></div>
+    <div className="font-[family-name:var(--font-geist-sans)]">
+      <Banner />
+
+      <AboutSection />
+
+      <ServicesSection />
+
+      <TestimonialsSection />
+    </div>
   );
 }
