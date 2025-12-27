b9a1e5c273b93b577dc86e9ebcdd5f927a03fe65
Sanjida Parven Alfe <sanjidaparvinalfe@gmail.com>
2025-12-27 15:01:04 +0600

    Updated

--- src/app/api/booking/delete/[id]/route.js
+++ src/app/api/booking/delete/[id]/route.js
@@ -3,14 +3,27 @@ import { ObjectId } from "mongodb";
 import { NextResponse } from "next/server";
 
 export const DELETE = async (request, { params }) => {
-  const { id } = params;
+
+  const { id } = await params;
 
   try {
     const db = await dbConnect("bookings");
+    
     const query = { _id: new ObjectId(id) };
     const result = await db.deleteOne(query);
 
-    return NextResponse.json({ status: true, result });
+    if (result.deletedCount > 0) {
+      return NextResponse.json({ 
+        status: true, 
+        message: "Booking Deleted Successfully", 
+        result 
+      });
+    } else {
+      return NextResponse.json({ 
+        status: false, 
+        message: "Booking not found or already deleted" 
+      });
+    }
   } catch (error) {
     return NextResponse.json({ status: false, message: error.message });
   }
