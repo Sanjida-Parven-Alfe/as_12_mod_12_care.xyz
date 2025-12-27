08761b38b2928d2fe99050de5c1c286bac08fb8d
Sanjida Parven Alfe <sanjidaparvinalfe@gmail.com>
2025-12-27 15:56:28 +0600

    uppdate

--- middleware.js
+++ middleware.js
@@ -1,19 +1,18 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
-  const path = request.nextUrl.pathname;
-  
+  const { pathname } = request.nextUrl;
+
   const token = request.cookies.get("next-auth.session-token") || request.cookies.get("__Secure-next-auth.session-token");
 
-  const isPublicPath = path === "/login" || path === "/register";
-  const isPrivatePath = path.startsWith("/my-bookings") || path.startsWith("/booking");
+  const isPublicPath = pathname === "/login" || pathname === "/register";
 
- 
+  const isPrivatePath = pathname.startsWith("/my-bookings") || pathname.startsWith("/booking");
+
   if (isPublicPath && token) {
     return NextResponse.redirect(new URL("/", request.nextUrl));
   }
 
-  
   if (isPrivatePath && !token) {
     return NextResponse.redirect(new URL("/login", request.nextUrl));
   }
@@ -21,12 +20,11 @@ export function middleware(request) {
   return NextResponse.next();
 }
 
-
 export const config = {
   matcher: [
     "/login",
     "/register",
-    "/my-bookings",
-    "/booking/:path*",
+    "/my-bookings/:path*", 
+    "/booking/:path*",     
   ],
 };
